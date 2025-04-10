8f265a45ed68b0491ac119feca75d3ea30f3007c
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-10 16:24:13 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner s = new Scanner(System.in);
+//        Prompt the user for the size of the sandwich (1 or 2):
+//        - 1: Regular: base price $5.45
+//        - 2: Large: base price $8.95
+        System.out.println("Size (1 or 2):");
+        int size = s.nextInt();
+//        Prompt the user for their age:
+        System.out.println("Age in years:");
+        int age = s.nextInt();
+        double basePrice = 5.45;
+        if( size == 2){
+            basePrice = 8.95;
         }
+        double discount = 0;
+//        - Student (17 years old or younger) — receive a 10% discount
+        if(age <=17){
+            discount = 0.1;
+        }
+//        - Seniors (65 years old or older) — receive a 20% discount
+        if(age >= 65){
+            discount = 0.2;
+        }
+        double discountAmount = basePrice * discount;
+        double finalPrice = basePrice - discountAmount;
+//        Display the cost of the sandwich to the screen
+        System.out.println(finalPrice);
     }
 }
